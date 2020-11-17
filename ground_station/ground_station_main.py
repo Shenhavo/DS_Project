b0234c1d1b9788bbbffb9d19f2ca7a72ab100aae
danblana <danblana@gmail.com>
2020-11-17 15:35:11 +0200

    1. bugfix for last img rows (mini-me) 2. saving also a bin file of the received img

--- ground_station/ground_station_main.py
+++ ground_station/ground_station_main.py
@@ -4,7 +4,9 @@ from datetime import datetime
 import os
 import subprocess
 import time
-# import cv2
+from PIL import Image
+import cv2
+import pickle
 
 GLOBAL_VERBOSITY            =   5
 
@@ -41,7 +43,7 @@ SSID = 'WINC1500_AP'
 def main():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    prnt(("date and time ="+ dt_string), 0)
+    prnt(("date and time =" + dt_string), 0)
     # define the name of the directory to be created
     path = r"outputs\\" + now.strftime("%d%m%Y-%H%M%S")
 
@@ -74,57 +76,58 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         pm.add_socket(s)
         pm.socket.connect((pm.host, pm.port))
+        #
+        # print("Are you ready to start? y/n\r\n")
+        # user_intput = input()
+        #
+        # if( user_intput == 'y'):
+        s.sendall(b'Start\r\n')
 
-        print("Are you ready to start? y/n\r\n")
-        user_intput = input()
+        while True:
 
-        if( user_intput == 'y'):
-            s.sendall(b'Start\r\n')
-
-            while True:
-
-                incoming_data = s.recv(SOF_SIZE_B)
-                print("SOF="+str(incoming_data))
-                if incoming_data[0] == FRAME_SOF:
-                    incoming_data = s.recv(FRAME_HEADER_WO_SOF_SIZE_B)
-                    incoming_sys_tick = FourBytesToUint32(incoming_data, 0)
-                    print("tick=" + str(incoming_sys_tick))
-                    if pm.frame_sys_tick == 0 or pm.frame_sys_tick == incoming_sys_tick:
-                        pm.frame_sys_tick = incoming_sys_tick
-                    else:
-                        print("Invalid systick arrived") # TODO: see if it handles missing property
-                        pm.clear_frame_properties()
-                        continue
-                    pm.frame_size = TwoBytesToUint16(incoming_data, 4)
-                    if pm.frame_size > FRAME_DATA_SIZE_B:  # means that there will be an additional packet
-                        if pm.frame_rx_buff == bytes(0):
-                            pm.frame_rx_buff = s.recv(FRAME_DATA_SIZE_B)
-                        else:
-                            pm.frame_rx_buff = pm.frame_rx_buff + s.recv(FRAME_DATA_SIZE_B)
-
-                    else: # means this is the last packet
-                        try:
-                            pm.frame_rx_buff = pm.frame_rx_buff + s.recv(pm.frame_size - FRAME_HEADER_SIZE_B)
-                        except:
-                            expected_frame_size = pm.frame_size - FRAME_HEADER_SIZE_B
-                            print("bad recv size: %d" % expected_frame_size)
-                        else:
-                            pm.save_jpeg(path)
-                            # pm.crop_cheat(pm.last_saved_img_path)
-                            delta_tick = pm.frame_sys_tick - pm.last_valid_frame_sys_tick
-                            print("sys_tick diff = %d [msec]" % delta_tick)
-                            pm.last_valid_frame_sys_tick = pm.frame_sys_tick
-                        # ===== cleaning:  ======
-                        pm.clear_frame_properties()
-
-                # elif incoming_data[0] == IMU_SOF:
-                #     pm.imu_rx_buff = s.recv(IMU_PACKET_SIZE_WO_HEADER)
-                #     pm.imu_sys_tick = FourBytesToUint32(pm.imu_rx_buff, 0) - IMU_SYSTICK_SHIFT_MSEC
-                #     print( "X" +str(pm.imu_sys_tick))
-                #     # pm.print_IMU_data(pm.imu_rx_buff)
-                #     # pm.clear_imu_properties()
+            incoming_data = s.recv(SOF_SIZE_B)
+            print("SOF="+str(incoming_data))
+            if incoming_data[0] == FRAME_SOF:
+                incoming_data = s.recv(FRAME_HEADER_WO_SOF_SIZE_B)
+                incoming_sys_tick = FourBytesToUint32(incoming_data, 0)
+                print("tick=" + str(incoming_sys_tick))
+                if pm.frame_sys_tick == 0 or pm.frame_sys_tick == incoming_sys_tick:
+                    pm.frame_sys_tick = incoming_sys_tick
                 else:
-                    prnt("ERR SOF", 1)
+                    print("invalid systick arrived")# TODO: see if it handles missing property
+                    pm.clear_frame_properties()
+                    continue
+                pm.frame_size = TwoBytesToUint16(incoming_data, 4)
+                if pm.frame_size > FRAME_DATA_SIZE_B:  # means that there will be an additional packet
+                    if pm.frame_rx_buff == bytes(0):
+                        pm.frame_rx_buff = s.recv(FRAME_DATA_SIZE_B)
+                    else:
+                        pm.frame_rx_buff = pm.frame_rx_buff + s.recv(FRAME_DATA_SIZE_B)
+
+                else: # means this is the last packet
+                    try:
+                        pm.frame_rx_buff = pm.frame_rx_buff + s.recv(pm.frame_size)# - FRAME_HEADER_SIZE_B)
+                    except:
+                        expected_frame_size = pm.frame_size - FRAME_HEADER_SIZE_B
+                        print("bad recv size: %d" % expected_frame_size)
+                    else:
+                        print("frame recv size: %d" % len(pm.frame_rx_buff))
+                        pm.save_jpeg(path)
+                        # pm.crop_cheat(pm.last_saved_img_path)
+                        delta_tick = pm.frame_sys_tick - pm.last_valid_frame_sys_tick
+                        print("sys_tick diff = %d [msec]" % delta_tick)
+                        pm.last_valid_frame_sys_tick = pm.frame_sys_tick
+                    # ===== cleaning:  ======
+                    pm.clear_frame_properties()
+
+            # elif incoming_data[0] == IMU_SOF:
+            #     pm.imu_rx_buff = s.recv(IMU_PACKET_SIZE_WO_HEADER)
+            #     pm.imu_sys_tick = FourBytesToUint32(pm.imu_rx_buff, 0) - IMU_SYSTICK_SHIFT_MSEC
+            #     print( "X" +str(pm.imu_sys_tick))
+            #     # pm.print_IMU_data(pm.imu_rx_buff)
+            #     # pm.clear_imu_properties()
+            else:
+                prnt("ERR SOF", 1)
         else:
             prnt("bye bye..\r\n")
             input()
@@ -180,11 +183,34 @@ class PacketMngr:
         # prnt("image size =", 0)
         # self.print_buff_len()
         # prnt(("img systick = " + str(self.frame_sys_tick)), 0)
-        self.last_saved_img_path = save_path + "\\" + str(self.frame_sys_tick) + ".jpeg"
-        self.ptr_jpeg = open(self.last_saved_img_path, 'w+b')
+        curr_saved_file_name = save_path + "\\" + str(self.frame_sys_tick)
+
+        self.last_saved_img_path = curr_saved_file_name + ".jpeg"
+        self.ptr_jpeg = open(self.last_saved_img_path, "wb")
         self.ptr_jpeg.write(self.frame_rx_buff)
         self.ptr_jpeg.close()
 
+        #TODO: remove
+        # self.last_saved_img_path = save_path + "\\" + str(self.frame_sys_tick) + ".bmp"
+        # self.ptr_jpeg = open(self.last_saved_img_path, 'w+b')
+        # self.ptr_jpeg.write(self.frame_rx_buff)
+        # self.ptr_jpeg.close()
+        f_bin_out_path = curr_saved_file_name + ".bin"
+        f_bin_out = open(f_bin_out_path, "wb")
+        f_bin_out.write(self.frame_rx_buff)
+        f_bin_out.close()
+
+        # fix_jpeg_save_path = save_path + "\\" + str(self.frame_sys_tick) + "fix.jpeg"
+        # img = cv2.imread(self.last_saved_img_path)
+        # cv2.imshow('image', img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        # crop_img = img[16:288, 0:240]
+        # cv2.imshow("cropped" + img_path, crop_img)
+        # cv2.imwrite(fix_jpeg_save_path, img)
+#        Image.open(self.last_saved_img_path).save("sample1.bmp")
+
+    # TOOD: remove
     def crop_cheat(self, img_path):
         img = cv2.imread(img_path)
         crop_img = img[16:288, 0:240]
